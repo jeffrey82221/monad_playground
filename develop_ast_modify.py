65e5d370e447ec1cfc621677da489e936a9ce472
jiangjiawei1103 <waynechuang97@gmail.com>
2022-08-03 11:28:22 +0800

    feat: Add in-class main_func parsing

--- develop_ast_modify.py
+++ develop_ast_modify.py
@@ -2,14 +2,17 @@
 TODO:
 - [ ] 1. change main_func -> target_main_func 
 - [ ] 2. assert that all lines should be x = self.xxx(x, x)
+
+https://stackoverflow.com/questions/972/
+https://greentreesnakes.readthedocs.io/en/latest/tofrom.html#fix-locations
 """
+from monad_parser import MonadParser
+
 class CustomizeProcess:
-    """
-    def target_main_func(self, a, b):
-        p1 = self.bind(self.sub_func_1_plus)(a, b)
-        p2 = self.bind(self.sub_func_2_prod)(a, b)
-        return p1, p2
-    """
+    
+    def __init__(self):
+        self.__parse_main_func()
+    
     def main_func(self, a, b):
         p1 = self.sub_func_1_plus(a, b)
         p2 = self.sub_func_2_prod(a, b)
@@ -23,4 +26,10 @@ class CustomizeProcess:
     
     def bind(self, func):
         return func
-            
+    
+    def __parse_main_func(self) -> None:
+        monad_parser = MonadParser(__file__)
+        target_main_func_str = monad_parser.parse()
+        print(target_main_func_str)
+        exec(target_main_func_str, globals())
+        self.target_main_func = target_main_func   # Bound method
