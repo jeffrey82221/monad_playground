1400a71987b3d95e5dbfd0581e279d43cd8f3c6c
jeffrey82221 <jeffrey82221@gmail.com>
2022-08-05 22:02:28 +0800

    debug: understand how to get typing in typing.Tuple

--- decorator_experiment.py
+++ decorator_experiment.py
@@ -1,4 +1,6 @@
 import inspect 
+from typing import Tuple
+import typing
 from functools import wraps 
 def bind(orig_func):
     '''decorator for allowing a function to support monad design pattern'''
@@ -12,21 +14,23 @@ def bind(orig_func):
     return wrapper
 
 
-def plus(a: int, b: int) -> (int, int):
-    return a + b, b
-
-print(plus.__annotations__.items())
+def plus(a: int, b: int) -> int:
+    return a + b
+print(plus.__annotations__['return'])
+print(type(plus.__annotations__['return']))
 @bind
-def plus(a: int, b: int) -> (int, int):
-    return a + b, b
+def plus(a: int, b: int) -> Tuple[int, int, int]:
+    return a + b, b, b
 
 print(plus.__annotations__.items())
 
 print(plus(1, 2))
 
+print(type(plus.__annotations__['return']))
+print(isinstance(plus.__annotations__['return'], typing._GenericAlias))
 
 print(plus.__annotations__['return'])
 
-print(len(plus.__annotations__['return']))
+print(plus.__annotations__['return'].__args__)
     
         
