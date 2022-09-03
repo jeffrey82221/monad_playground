e73c265b67f689e73d327e0832a767c4be255265
jeffrey82221 <jeffrey82221@gmail.com>
2022-09-03 10:46:48 +0800

    refactor: clean up dead message

--- experiment/conditional_inherence.py
+++ experiment/conditional_inherence.py
@@ -1,4 +1,7 @@
-from ast import Not
+"""
+selectively inherit `A` or `B` class via `AorBBase`
+with switch defined on the `mode` method at the direct subclass of `AorBBase`
+"""
 
 
 class A(object):
@@ -22,25 +25,8 @@ class B(object):
 
 
 class AorB(type):
-    """ A Metaclass which decorates all the methods of the
-        subclass using call_counter as the decorator
+    """ A Metaclass which decorates the entire class
     """
-    """
-    @staticmethod
-    def some_decorator(func):
-        '''Decorator for counting the number of function
-            or method calls to the function or method func
-            
-        '''
-        def helper(*args, **kwargs):
-            helper.calls += 1
-            return func(*args, **kwargs)
-        helper.calls = 0
-        helper.__name__ = func.__name__
-
-        return helper
-    """
-
     def __new__(cls, clsname, superclasses, attributedict):
         """
         This is like a decorator to a class
@@ -48,25 +34,27 @@ class AorB(type):
         print('cls:', cls)
         print('clsname', clsname)
         print('superclasses before', superclasses)
-        # print('attributedict', attributedict)
-        if 'mode' in attributedict and 'AorBBase' in [c.__name__ for c in list(superclasses)]:
+        if 'mode' in attributedict and 'AorBBase' in [
+                c.__name__ for c in list(superclasses)]:
             switch_label = attributedict['mode']('self')
             print('switch_label', switch_label)
             if switch_label == 'A':
-                # Only A as superclass
+                # add A as superclass
                 superclasses = list(superclasses)
                 superclasses.append(A)
                 superclasses = tuple(superclasses)
             elif switch_label == 'B':
-                # Only B as superclass
+                # add B as superclass
                 superclasses = list(superclasses)
                 superclasses.append(B)
                 superclasses = tuple(superclasses)
-            # apply mode attribute to superclasses
         print('superclasses after:', superclasses)
         return super().__new__(cls, clsname, superclasses, attributedict)
 
+
 print('BaseC ################')
+
+
 class AorBBase(metaclass=AorB):
     def c_method(self):
         print('base c')
@@ -74,7 +62,10 @@ class AorBBase(metaclass=AorB):
     def mode(self):
         pass
 
+
 print('C ################')
+
+
 class CinhA(AorBBase):
     def c_method(self):
         print('c method')
@@ -82,20 +73,26 @@ class CinhA(AorBBase):
     def mode(self):
         return 'A'
 
+
 print('D ################')
+
+
 class DinhB(AorBBase):
     def d_method(self):
         print('d method')
+
     def mode(self):
         return 'B'
 
+
 print('E ################')
+
+
 class E(DinhB):
-    # NOTE: mode in this level should not effect the selection of inheritance 
+    # NOTE: mode in this level should not effect the selection of inheritance
     # at the level of class inherit BaseC
     def mode(self):
         return 'A'
-
 
 
 if __name__ == '__main__':
--- experiment/nested_group_selective_inheritence.py
+++ experiment/nested_group_selective_inheritence.py
@@ -1,14 +1,13 @@
 """
-TODO:
 
-Analysis: 
+Analysis:
 
 1. NestProcess (has/hasnot operations) -> NestedBase (has operations) -> Nested -> Monad
 2. GroupProcess (might has operations) -> GroupBase (has no operations) -> Group -> Monad
 
-NOTE:
+Summary:
 
-- [ ] Operations method at NestProcess & GroupProcess should not affect 
+- NOTE: Operations method at NestProcess & GroupProcess should not affect
 the selective inheritence at the level of NestedBase & GroupBase
 
 """
